f9dad35e16ff64473de4ff77512d91b83c82d7d1
Miłosz Mościcki <miloszmoscicki@mioszs-mbp.home>
2019-11-17 21:45:20 +0000

    test

---
+++ ByteCoin/Model/CurrencyData.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyData.swift
+//  ByteCoin
+//
+//  Created by Miłosz Mościcki on 17/11/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CurrencyData: Codable{
+    let last: Float
+}
