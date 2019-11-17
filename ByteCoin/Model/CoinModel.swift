ab873b29801cfef366a78b41d20c373ba0944c4e
Miłosz Mościcki <miloszmoscicki@mioszs-mbp.home>
2019-11-17 21:39:26 +0000

    test

---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Miłosz Mościcki on 17/11/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+   let rate: Float
+   let currency: String
+}
